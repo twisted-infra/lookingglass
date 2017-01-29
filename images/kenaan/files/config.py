f8afe31f087f4fc2b0366473cbecfe6a3c534917
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2017-01-29 14:46:07 +1100

    kenaan, trac, fix amptrac

---
+++ images/kenaan/files/config.py
@@ -0,0 +1,35 @@
+# Hostname where persistent bot process is listening
+BOT_HOST = 'twistedmatrix.com'
+
+# Port number of same
+BOT_PORT = 15243
+
+# Where to talk to amptrac
+AMPTRAC_PORT="tcp:host=amptrac:port=1352"
+
+# Maximum number of characters to put into a single IRC message
+LINE_LENGTH = 400
+
+# Delay, in seconds, between IRC messages
+LINE_RATE = 1.0
+
+# Two-tuples of (tracker URL, channel) defining the rules for announcing
+# ticket changes.
+TICKET_RULES = [
+    ('http://twistedmatrix.com/trac/', ['#twisted', '#twisted-dev']),
+    ]
+
+# The channel to which to send alerts.
+ALERT_CHANNEL = '#twisted-admin'
+
+# The directory in which log files will be written.  Most users can
+# write to their own home directory, but some can't - eg www-data.
+# So, give each user a directory in /tmp.  They can't use /tmp itself
+# because users won't be able to open log files written by other
+# users.
+import os, pwd, tempfile
+LOG_ROOT = os.path.join(
+    tempfile.gettempdir(),
+    'kenaan-logs-' + pwd.getpwuid(os.getuid()).pw_name)
+if not os.path.isdir(LOG_ROOT):
+    os.makedirs(LOG_ROOT)
